ce3c173a07be02ca9600e7b282f2d91717d84274
Lyle Wong <wlyle473@gmail.com>
2024-02-28 14:12:20 +0000

    Added "base" code: - Can run the server and see a text on the screen.

---
+++ FirstYearHack/myApp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+# Paths that will connect a URL pattern to a specific path or view
+urlpatterns = [
+                path("", views.home, name="home") # Base URL of website
+                ]
