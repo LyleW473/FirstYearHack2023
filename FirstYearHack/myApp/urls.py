2cf384e967d27beb8946def97743c748598c30e2
Lyle Wong <wlyle473@gmail.com>
2024-02-28 14:35:10 +0000

    Added new page: - For to do list (personal tracker requirement).

--- FirstYearHack/myApp/urls.py
+++ FirstYearHack/myApp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 # Paths that will connect a URL pattern to a specific path or view
 urlpatterns = [
-                path("", views.home, name="home") # Base URL of website
+                path("", views.home, name="home"), # Base URL of website
+                path("todos/", views.todos, name="todos")
                 ]
--- FirstYearHack/myApp/views.py
+++ FirstYearHack/myApp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, HttpResponse
+from .models import ToDoItem
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html") # Return html template
+    return render(request=request, template_name="home.html") # Return html template
+
+def todos(request):
+    items = ToDoItem.objects.all() # Get all instances of ToDoItem in the database
+    return render(request=request, template_name="todos.html", context = {"todos": items})
